6f318183deb25d6aa1ef74ddecb60e02fff7c9ba
edhale3 <edhale33@gmail.com>
2020-10-09 00:21:19 -0700

    Made some changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 
 app.get("/", (req,res) => {
@@ -9,10 +10,10 @@ app.get("/", (req,res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
     app.get('/*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'calculator.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'calculator.html'));
     });
   }
 
